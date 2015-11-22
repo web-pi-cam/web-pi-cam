a9c5f89662c8961ab45ee77a9bd7fa4588671ac8
Kevin <kevinahuber@gmail.com>
2015-11-22 15:58:26 -0600

    Test image

--- server.py
+++ server.py
@@ -2,16 +2,26 @@
 import os
 
 from flask import Flask, send_file
-import picamera
+try:
+    import picamera
+except:
+    pass
 
 app = Flask(__name__)
 
 @app.route("/picture")
 def take_picture():
-    with picamera.PiCamera() as camera:
-        filename = "temp.jpg"
-        camera.capture(filename)
-        return send_file(filename, mimetype="image/jpg")
+    try:
+        with picamera.PiCamera() as camera:
+            filename = "temp.jpg"
+            camera.capture(filename)
+            return send_file(filename, mimetype="image/jpg")
+    except:
+        return send_file('/images/test-image.jpg')
+
+@app.route("/")
+def render_picture():
+    return render_template('/templates/show_camera_stream.html', picture='/picture')
 
 if __name__ == '__main__':
     # TODO: Don't alays expose to whole internet
