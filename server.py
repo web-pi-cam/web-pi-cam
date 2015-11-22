f6012846eea35e6126f00d103803d36686737b74
Max Shenfield <shenfieldmax@gmail.com>
2015-11-22 15:28:21 -0600

    Untested video streaming functionality

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 """Handle incoming requests and send back the picture"""
-import os
+import io
 
-from flask import Flask, send_file
+from flask import Flask, Response, send_file
 import picamera
+from camera_pi import Camera
 
 app = Flask(__name__)
 
@@ -12,6 +13,18 @@ def take_picture():
         filename = "temp.jpg"
         camera.capture(filename)
         return send_file(filename, mimetype="image/jpg")
+
+def generate_video(camera):
+    '''Simulate streaming security-camera video'''
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route("/video")
+def stream_video():
+    return Response(generate_video(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     # TODO: Don't alays expose to whole internet
