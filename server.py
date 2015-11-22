242b83a6ea362bdec0da2629912e779bf01d178f
Max Shenfield <shenfieldmax@gmail.com>
2015-11-22 14:06:31 -0600

    Run server with option to disable debug

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 """Handle incoming requests and send back the picture"""
+import os
+
 from flask import Flask, send_file
 import picamera
 
 app = Flask(__name__)
+app.debug = os.env.get("WEB_PI_CAM_DEBUG", True)
 
 @app.route("/picture")
 def take_picture():
@@ -10,3 +13,6 @@ def take_picture():
         filename = "temp.jpg"
         camera.capture(filename)
         send_file(filename, mimetype="image/jpg")
+
+if __name__ == '__main__':
+    app.run()
