d1a602ee639c939e85e6e2380b4f02e37dfd5c00
Kevin <kevinahuber@gmail.com>
2015-12-01 23:07:00 -0600

    Added video stream

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ except:
     print('Camera not found')
     pass
 app = Flask(__name__)
-
+stream = io.BytesIO()
 @app.route("/picture")
 def take_picture():
     try:
@@ -27,11 +27,14 @@ def render_picture():
     return render_template('/show_camera_stream.html')
 
 def generate_video(camera):
-    '''Simulate streaming security-camera video'''
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    camera.start_recording(stream, format='h264', quality=23)
+    camera.wait_recording(15)
+    camera.stop_recording()
+    # '''Simulate streaming security-camera video'''
+    # while True:
+    #     frame = camera.get_frame()
+    #     yield (b'--frame\r\n'
+    #            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route("/video")
 def stream_video():
