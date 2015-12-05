f47df5da7923af10048a140f30417ee752e5f421
Kevin <kevinahuber@gmail.com>
2015-12-04 21:24:56 -0600

    Revert to frames, with pi camera`

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ except:
     print('Camera not found')
     pass
 app = Flask(__name__)
-stream = io.BytesIO()
 @app.route("/picture")
 def take_picture():
     try:
@@ -27,21 +26,17 @@ def render_picture():
     return render_template('/show_camera_stream.html')
 
 def generate_video(camera):
-    camera.start_recording(stream, format='h264', quality=23)
-    camera.wait_recording(15)
-    camera.stop_recording()
-    # '''Simulate streaming security-camera video'''
-    # while True:
-    #     frame = camera.get_frame()
-    #     yield (b'--frame\r\n'
-    #            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    '''Simulate streaming security-camera video'''
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route("/video")
 def stream_video():
     with picamera.PiCamera() as camera:
-        return Response(generate_video(camera))
-    # return Response(generate_video(Camera()),
-    #                 mimetype='multipart/x-mixed-replace; boundary=frame')
+        return Response(generate_video(camera),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     # TODO: Don't alays expose to whole internet
