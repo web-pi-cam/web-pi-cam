309172caae067c58c21a7b2ff33a3354403f95d4
Kevin <kevinahuber@gmail.com>
2015-12-25 23:33:20 -0500

    Change location of camera capture

--- server.py
+++ server.py
@@ -1,13 +1,9 @@
 """Handle incoming requests and send back the picture"""
 import io
 
-from flask import Flask, Response, send_file, render_template
-from time import sleep
-from datetime import datetime
-from fractions import Fraction
+from flask import Flask, send_file, render_template
 try:
     import picamera
-    from camera_pi import Camera
 except:
     print('Camera not found')
     pass
@@ -15,16 +11,14 @@ app = Flask(__name__)
 
 @app.route("/picture")
 def take_picture():
-    try:
-        with picamera.PiCamera() as camera:
-            camera.resolution = (1024, 1024)
-            filename = "temp.jpg"
-            camera.capture(filename)
-            print open('./images/'+ filename, "w")
-            return send_file(filename, mimetype="image/jpg")
-    except:
-        return send_file('./images/test-image.jpeg')
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 1024)
+        filename = "temp.jpg"
+        camera.capture(filename)
+        print open('./images/'+ filename, "w")
 
+# TODO: Take picture on every capture
+# TODO: Download picture rather than send picture on mobile.
 @app.route("/get_picture")
 def get_picture():
     return send_file('temp.jpg')
@@ -35,21 +29,5 @@ def render_picture():
 
 @app.route("/capture")
 def render_capture():
+    take_picture()
     return render_template('show_picture.html')
-
-def generate_video(camera):
-    '''Simulate streaming security-camera video'''
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route("/video")
-def stream_video():
-    return Response(generate_video(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    # TODO: Don't alays expose to whole internet
-    app.debug = True
-    app.run(host="0.0.0.0")
